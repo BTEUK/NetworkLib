360c391c5c3a7747604043375560303947d30431
ELgamer <laurens.wolfert@gmail.com>
2024-05-11 11:50:37 +0200

    No channels

--- src/main/java/net/bteuk/network/lib/dto/AbstractTransferObject.java
+++ src/main/java/net/bteuk/network/lib/dto/AbstractTransferObject.java
@@ -16,8 +16,6 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
         @JsonSubTypes.Type(value = DiscordLinking.class, name = "DISCORD_LINKING"),
         @JsonSubTypes.Type(value = DiscordRole.class, name = "DISCORD_ROLE"),
         @JsonSubTypes.Type(value = TabEvent.class, name = "TAB_EVENT"),
-        @JsonSubTypes.Type(value = ChannelEvent.class, name = "CHANNEL_EVENT"),
-        @JsonSubTypes.Type(value = ChannelJoin.class, name = "CHANNEL_JOIN"),
         @JsonSubTypes.Type(value = PlayerConnect.class, name = "PLAYER_CONNECT"),
         @JsonSubTypes.Type(value = PlayerDisconnect.class, name = "PLAYER_DISCONNECT")
 })
