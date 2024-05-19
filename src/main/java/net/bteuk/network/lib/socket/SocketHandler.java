1c603b12378ae9e06f8292f4a0a377bef3f954aa
ELgamer <laurens.wolfert@gmail.com>
2024-05-19 11:41:57 +0200

    Reply object is also AbstractTransferObject

--- src/main/java/net/bteuk/network/lib/socket/SocketHandler.java
+++ src/main/java/net/bteuk/network/lib/socket/SocketHandler.java
@@ -1,10 +1,9 @@
 package net.bteuk.network.lib.socket;
 
 import net.bteuk.network.lib.dto.AbstractTransferObject;
-import net.bteuk.network.lib.dto.Reply;
 
 public interface SocketHandler {
 
-    Reply handle(AbstractTransferObject transferObject);
+    AbstractTransferObject handle(AbstractTransferObject transferObject);
 
 }
