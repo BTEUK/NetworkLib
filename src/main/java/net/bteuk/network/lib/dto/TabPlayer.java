d0e521146f66bd6c6d5c42f26bacffaeda0782bb
ELgamer <laurens.wolfert@gmail.com>
2024-05-20 23:08:24 +0200

    The prefix is not necessary in the backend, the server can get the prefix based on the role name.
    This also removes the need for a custom (de)serializer.

--- src/main/java/net/bteuk/network/lib/dto/TabPlayer.java
+++ src/main/java/net/bteuk/network/lib/dto/TabPlayer.java
@@ -1,20 +1,13 @@
 package net.bteuk.network.lib.dto;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import net.bteuk.network.lib.deserializer.TabPlayerDeserializer;
-import net.bteuk.network.lib.serializer.TabPlayerSerializer;
-import net.kyori.adventure.text.Component;
 
 /**
  * Tab Player
  */
-@JsonDeserialize(using = TabPlayerDeserializer.class)
-@JsonSerialize(using = TabPlayerSerializer.class)
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -24,8 +17,6 @@ public class TabPlayer {
     private String uuid;
 
     private String name;
-
-    private Component prefix;
 
     /** The primary group of the player. */
     private String primaryGroup;
