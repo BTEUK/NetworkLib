a2cd32bfe6ed236f84f5686abee0dff5de752691
ELgamer <laurens.wolfert@gmail.com>
2024-07-13 14:58:10 +0200

    Add a requiredArgsConstructor to DirectMessage so you can still create the object without including the default false offline variable.

--- src/main/java/net/bteuk/network/lib/dto/DirectMessage.java
+++ src/main/java/net/bteuk/network/lib/dto/DirectMessage.java
@@ -6,25 +6,26 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import net.bteuk.network.lib.deserializer.ComponentDeserializer;
 import net.bteuk.network.lib.serializer.ComponentSerializer;
 import net.kyori.adventure.text.Component;
 
 @JsonTypeName("DIRECT_MESSAGE")
-@NoArgsConstructor
 @AllArgsConstructor
+@RequiredArgsConstructor
 @Getter
 @Setter
 public class DirectMessage extends AbstractTransferObject {
 
-    private String recipient;
+    private final String recipient;
 
-    private String sender;
+    private final String sender;
 
     @JsonSerialize(using = ComponentSerializer.class)
     @JsonDeserialize(using = ComponentDeserializer.class)
-    private Component component;
+    private final Component component;
 
     /** Whether the message should also be sent if the player is offline. */
     private boolean offline = false;
