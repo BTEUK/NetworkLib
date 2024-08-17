3cb0a48f5bdf13a367d19088367be8f285f2214a
ELgamer <laurens.wolfert@gmail.com>
2024-08-17 22:54:42 +0200

    Class has no fields, so AllArgsConstructor is obsolete.

--- src/main/java/net/bteuk/network/lib/dto/OnlineUsersRequest.java
+++ src/main/java/net/bteuk/network/lib/dto/OnlineUsersRequest.java
@@ -1,14 +1,12 @@
 package net.bteuk.network.lib.dto;
 
 import com.fasterxml.jackson.annotation.JsonTypeName;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @JsonTypeName("ONLINE_USERS_REQUEST")
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
 public class OnlineUsersRequest extends AbstractTransferObject {
